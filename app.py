0e9e6000552646140344475307513ef3a8a2841a
Kuldeep Kushwah <kuldeepkushwah0509@gmail.com>
2026-09-21 19:30:10 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ import sqlite3
 import urllib.parse
 import re
 import random
+USER_AGENT = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36"
 
 app = Flask(__name__)
 app.secret_key = "pricemint_super_secret_key_2026"
